6c10aec17459ad40e1d0840c78529b5f05cbf3fd
Temah Finger Of God <temahfingerofgod@gmail.com>
2025-08-28 13:25:35 +0000

    Add more payment method like; Moble money(ie., telecel cash and MTN momo

--- src/app/(main)/checkout/page.tsx
+++ src/app/(main)/checkout/page.tsx
@@ -13,7 +13,10 @@ import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle }
 import { Separator } from '@/components/ui/separator';
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@/components/ui/select';
 import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from '@/components/ui/form';
+import { RadioGroup, RadioGroupItem } from '@/components/ui/radio-group';
 import Image from 'next/image';
+import { CreditCard, Truck, Smartphone } from 'lucide-react';
+import { useState } from 'react';
 
 const shippingSchema = z.object({
   fullName: z.string().min(3, 'Full name is required'),
@@ -24,13 +27,23 @@ const shippingSchema = z.object({
   country: z.string().min(2, 'Country is required'),
 });
 
-const paymentSchema = z.object({
-  cardNumber: z.string().regex(/^\d{16}$/, 'Card number must be 16 digits'),
-  expiryDate: z.string().regex(/^(0[1-9]|1[0-2])\/\d{2}$/, 'Expiry date must be in MM/YY format'),
-  cvv: z.string().regex(/^\d{3,4}$/, 'CVV must be 3 or 4 digits'),
-});
+const checkoutSchema = z.discriminatedUnion("paymentMethod", [
+  z.object({
+    paymentMethod: z.literal("card"),
+    cardNumber: z.string().regex(/^\d{16}$/, 'Card number must be 16 digits'),
+    expiryDate: z.string().regex(/^(0[1-9]|1[0-2])\/\d{2}$/, 'Expiry date must be in MM/YY format'),
+    cvv: z.string().regex(/^\d{3,4}$/, 'CVV must be 3 or 4 digits'),
+  }),
+  z.object({
+    paymentMethod: z.literal("mobile_money"),
+    mobileMoneyProvider: z.enum(["mtn", "telecel"], { required_error: "Please select a provider" }),
+    mobileMoneyNumber: z.string().min(10, 'Please enter a valid phone number'),
+  }),
+  z.object({
+    paymentMethod: z.literal("on_delivery"),
+  }),
+]).and(shippingSchema);
 
-const checkoutSchema = shippingSchema.merge(paymentSchema);
 
 type CheckoutFormValues = z.infer<typeof checkoutSchema>;
 
@@ -38,11 +51,14 @@ export default function CheckoutPage() {
   const { state: cartState, dispatch: cartDispatch } = useCart();
   const { items } = cartState;
   const { toast } = useToast();
+  const [selectedPaymentMethod, setSelectedPaymentMethod] = useState<'card' | 'mobile_money' | 'on_delivery'>('card');
+
 
   const form = useForm<CheckoutFormValues>({
     resolver: zodResolver(checkoutSchema),
     defaultValues: {
-      country: 'USA'
+      country: 'USA',
+      paymentMethod: 'card',
     }
   });
 
@@ -153,6 +169,7 @@ export default function CheckoutPage() {
                           <SelectItem value="USA">United States</SelectItem>
                           <SelectItem value="CAN">Canada</SelectItem>
                           <SelectItem value="MEX">Mexico</SelectItem>
+                           <SelectItem value="GHA">Ghana</SelectItem>
                         </SelectContent>
                       </Select>
                       <FormMessage />
@@ -164,49 +181,139 @@ export default function CheckoutPage() {
             
             <Card>
               <CardHeader>
-                <CardTitle>Payment Information</CardTitle>
-                <CardDescription>All transactions are secure and encrypted.</CardDescription>
+                <CardTitle>Payment Method</CardTitle>
+                <CardDescription>Choose how you'd like to pay for your order.</CardDescription>
               </CardHeader>
-              <CardContent className="grid grid-cols-1 md:grid-cols-4 gap-4">
-                 <FormField
+              <CardContent>
+                <FormField
                   control={form.control}
-                  name="cardNumber"
+                  name="paymentMethod"
                   render={({ field }) => (
-                    <FormItem className="md:col-span-4">
-                      <FormLabel>Card Number</FormLabel>
-                      <FormControl>
-                        <Input placeholder="0000 0000 0000 0000" {...field} />
-                      </FormControl>
-                      <FormMessage />
+                    <FormItem>
+                       <FormControl>
+                         <RadioGroup
+                          onValueChange={(value) => {
+                            field.onChange(value);
+                            setSelectedPaymentMethod(value as any);
+                          }}
+                          defaultValue={field.value}
+                          className="grid grid-cols-1 md:grid-cols-3 gap-4"
+                        >
+                          <FormItem>
+                            <Label className="flex flex-col items-center justify-center rounded-md border-2 border-muted bg-popover p-4 hover:bg-accent hover:text-accent-foreground peer-data-[state=checked]:border-primary [&:has([data-state=checked])]:border-primary cursor-pointer">
+                              <RadioGroupItem value="card" id="card" className="peer sr-only" />
+                              <CreditCard className="mb-3 h-6 w-6"/>
+                              Credit/Debit Card
+                            </Label>
+                          </FormItem>
+                          <FormItem>
+                            <Label className="flex flex-col items-center justify-center rounded-md border-2 border-muted bg-popover p-4 hover:bg-accent hover:text-accent-foreground peer-data-[state=checked]:border-primary [&:has([data-state=checked])]:border-primary cursor-pointer">
+                              <RadioGroupItem value="mobile_money" id="mobile_money" className="peer sr-only" />
+                              <Smartphone className="mb-3 h-6 w-6" />
+                              Mobile Money
+                            </Label>
+                          </FormItem>
+                          <FormItem>
+                            <Label className="flex flex-col items-center justify-center rounded-md border-2 border-muted bg-popover p-4 hover:bg-accent hover:text-accent-foreground peer-data-[state=checked]:border-primary [&:has([data-state=checked])]:border-primary cursor-pointer">
+                                <RadioGroupItem value="on_delivery" id="on_delivery" className="peer sr-only" />
+                                <Truck className="mb-3 h-6 w-6"/>
+                                Pay on Delivery
+                            </Label>
+                          </FormItem>
+                         </RadioGroup>
+                       </FormControl>
+                       <FormMessage className="pt-4" />
                     </FormItem>
                   )}
                 />
-                 <FormField
-                  control={form.control}
-                  name="expiryDate"
-                  render={({ field }) => (
-                    <FormItem className="md:col-span-2">
-                      <FormLabel>Expiry Date</FormLabel>
-                      <FormControl>
-                        <Input placeholder="MM/YY" {...field} />
-                      </FormControl>
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
-                 <FormField
-                  control={form.control}
-                  name="cvv"
-                  render={({ field }) => (
-                    <FormItem className="md:col-span-2">
-                      <FormLabel>CVV</FormLabel>
-                      <FormControl>
-                        <Input placeholder="123" {...field} />
-                      </FormControl>
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
+              </CardContent>
+              <CardContent>
+                {selectedPaymentMethod === 'card' && (
+                   <div className="grid grid-cols-1 md:grid-cols-4 gap-4">
+                     <FormField
+                      control={form.control}
+                      name="cardNumber"
+                      render={({ field }) => (
+                        <FormItem className="md:col-span-4">
+                          <FormLabel>Card Number</FormLabel>
+                          <FormControl>
+                            <Input placeholder="0000 0000 0000 0000" {...field} />
+                          </FormControl>
+                          <FormMessage />
+                        </FormItem>
+                      )}
+                    />
+                     <FormField
+                      control={form.control}
+                      name="expiryDate"
+                      render={({ field }) => (
+                        <FormItem className="md:col-span-2">
+                          <FormLabel>Expiry Date</FormLabel>
+                          <FormControl>
+                            <Input placeholder="MM/YY" {...field} />
+                          </FormControl>
+                          <FormMessage />
+                        </FormItem>
+                      )}
+                    />
+                     <FormField
+                      control={form.control}
+                      name="cvv"
+                      render={({ field }) => (
+                        <FormItem className="md:col-span-2">
+                          <FormLabel>CVV</FormLabel>
+                          <FormControl>
+                            <Input placeholder="123" {...field} />
+                          </FormControl>
+                          <FormMessage />
+                        </FormItem>
+                      )}
+                    />
+                  </div>
+                )}
+                 {selectedPaymentMethod === 'mobile_money' && (
+                   <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+                      <FormField
+                        control={form.control}
+                        name="mobileMoneyProvider"
+                        render={({ field }) => (
+                          <FormItem>
+                            <FormLabel>Provider</FormLabel>
+                            <Select onValueChange={field.onChange} defaultValue={field.value}>
+                              <FormControl>
+                                <SelectTrigger>
+                                  <SelectValue placeholder="Select a provider" />
+                                </SelectTrigger>
+                              </FormControl>
+                              <SelectContent>
+                                <SelectItem value="mtn">MTN Momo</SelectItem>
+                                <SelectItem value="telecel">Telecel Cash</SelectItem>
+                              </SelectContent>
+                            </Select>
+                            <FormMessage />
+                          </FormItem>
+                        )}
+                      />
+                      <FormField
+                          control={form.control}
+                          name="mobileMoneyNumber"
+                          render={({ field }) => (
+                              <FormItem>
+                                  <FormLabel>Mobile Number</FormLabel>
+                                  <FormControl>
+                                      <Input placeholder="024 123 4567" {...field} />
+                                  </FormControl>
+                                  <FormMessage />
+                              </FormItem>
+                          )}
+                      />
+                   </div>
+                )}
+                {selectedPaymentMethod === 'on_delivery' && (
+                  <div className="text-center text-muted-foreground bg-gray-50 p-4 rounded-md">
+                    You will pay with cash or mobile money when your order is delivered.
+                  </div>
+                )}
               </CardContent>
             </Card>
           </div>
@@ -262,4 +369,3 @@ export default function CheckoutPage() {
     </div>
   );
 }
-
