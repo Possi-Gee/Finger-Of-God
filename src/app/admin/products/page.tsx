4d15323ef1271976145ab46558150bb916069037
Temah Finger Of God <temahfingerofgod@gmail.com>
2025-08-28 13:12:55 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/admin/products/page.tsx
+++ src/app/admin/products/page.tsx
@@ -18,6 +18,7 @@ import {
   DialogFooter,
   DialogHeader,
   DialogTitle,
+  DialogTrigger,
 } from '@/components/ui/dialog';
 import {
   AlertDialog,
@@ -558,5 +559,3 @@ export default function AdminProductsPage() {
     </div>
   );
 }
-
-    
