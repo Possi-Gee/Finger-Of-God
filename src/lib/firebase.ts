05d00457d7d91d3ba03586f32f0eb1268c2c3eaa
Temah Finger Of God <temahfingerofgod@gmail.com>
2025-09-01 21:54:17 +0000

    It says it is already inside

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -9,7 +9,7 @@ const firebaseConfig = {
   "appId": "1:800300007748:web:627f0cb6d09321d989d711",
   "storageBucket": "shopwave-6mh7a.firebasestorage.app",
   "apiKey": "AIzaSyAvD2Tz2nEYI80Ni4Po2mKjDdepNwsNJxU",
-  "authDomain": "shopwave.firebaseapp.com",
+  "authDomain": "shopwave-6mh7a.firebaseapp.com",
   "measurementId": "",
   "messagingSenderId": "800300007748"
 };
